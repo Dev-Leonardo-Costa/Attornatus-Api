301d7ec62e8b9e2c8aa78536aacdb1107cd9dfd0
Leonardo Costa <devleonardo27@gmail.com>
2023-01-03 18:06:40 -0300

    Initial commit

---
+++ src/test/java/com/attornatus/AttornatusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attornatus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttornatusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
