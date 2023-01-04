528e137671c362d07582915f55f0d6c584bbca7f
Leonardo Costa <devleonardo27@gmail.com>
2023-01-04 11:06:44 -0300

    implementação do modelo endereço e repository,import sql para povoa os campos da tabela pessoa, endpoint de listar pessoa.

---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+insert into people(ID, NAME, DATE_BIRTH) values (1, 'Leonardo', '1993-05-12')
+insert into people(ID, NAME, DATE_BIRTH) values (2, 'Elisane', '1983-03-15')
+insert into people(ID, NAME, DATE_BIRTH) values (3, 'Fabio', '1989-05-10')
+insert into people(ID, NAME, DATE_BIRTH) values (4, 'Mario', '1993-12-07')
+
+--insert into address(ID,CITY, NUMBER, PLACE, ZIP_CODE,PEOPLE_ID) VALUES (1, 'FORTALEZA' ,4545, 'RUA GERALDO', '789789-045', 1)
